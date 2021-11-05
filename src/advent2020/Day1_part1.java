f2c0a9edf18613bbbbfd3e1ee4a9143c719589bb
Quipcore <felix.lido@gmail.com>
2021-11-05 23:53:37 +0100

    Adding day1 part2

---
+++ src/advent2020/Day1_part1.java
@@ -0,0 +1,44 @@
+package advent2020;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Day1_part1{
+	
+	//https://adventofcode.com/2020/day/1
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		File file = new File("D:\\Eclipse Workspace\\AdventOfCode\\src\\advent2020\\InputDay1");
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		String line;
+		int[] inputdata = new int[200];
+		int c = 0;
+		while ((line = br.readLine()) != null) {
+			inputdata[c] = Integer.valueOf(line);
+			c++;		
+		}
+		fr.close();
+		
+		int summa = 0;
+		int produkt = 0;
+		for(int i = 0; i < inputdata.length; i++) {
+			for(int j = 0; j < inputdata.length; j++)
+			{
+				summa = inputdata[i] + inputdata[j];
+				if (summa == 2020) {
+					produkt = inputdata[i]*inputdata[j];
+					System.out.println(inputdata[i] + " + " + inputdata[j] + " = " + summa);
+					System.out.println(inputdata[i] + " * " + inputdata[j] + " = " + produkt);
+					return;
+				}
+			}
+		}
+		
+		
+	}
+
+}
--- src/advent2020/Day1_part2.java
+++ src/advent2020/Day1_part2.java
@@ -1,6 +1,6 @@
 package advent2020;
 
-public class Day1_Report_Repair {
+public class Day1_part2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
